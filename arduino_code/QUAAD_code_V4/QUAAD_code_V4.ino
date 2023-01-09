426e4f6bb0f0f60a421b6f4ebc2ada46150336f5
Michael Parks <mipar@microsoft.com>
2023-01-09 17:00:28 +0000

    legibility

--- arduino_code/QUAAD_code_V4/QUAAD_code_V4.ino
+++ arduino_code/QUAAD_code_V4/QUAAD_code_V4.ino
@@ -36,73 +36,94 @@ int pattern[4] = {1, 1, 1, 1};
 int seq_step = 1;
 
 void setup() {
-analogReference(DEFAULT);
-pinMode(pin_CLOCK,INPUT);
-for (int i = 0; i <= 3; i++){
-pinMode(pin_A[i],OUTPUT); pinMode(pin_B[i],OUTPUT); pinMode(pin_CLK_DIV_OUT[i],OUTPUT);
-//pinMode(pin_PTRN[i],INPUT); pinMode(pin_CLK_DIV_IN[i],INPUT);
-clock_div_read = analogRead(pin_CLK_DIV_IN[i]);
-    mapped_div_ind   = constrain(map(clock_div_read,0,922,0,9),0,9);
-    division[i]      = divisions[mapped_div_ind];
-    clock_div_mid[i] = 51+(1024/10)*mapped_div_ind;
-}
-}
-
-void loop() {
-// Reset input
-if (digitalRead(pin_RESET) == HIGH){
-  if (wasReset == false){
-    clockCount = 0;
-    wasReset = true;}}
-else {wasReset = false;}
-  
-// Clock input
-curr_clock_state = digitalRead(pin_CLOCK);
-if      (curr_clock_state == 1 & last_clock_state == 0){went_up   = true;
-                                                        if(clockCount<3360){clockCount++;} //3360 can be divided by the numbers in divisions[]
-                                                        else               {clockCount=1;}}
-else if (curr_clock_state == 0 & last_clock_state == 1){went_down = true;}
-else                                                   {went_up = false; went_down = false;}
-last_clock_state = curr_clock_state;
-
-
-// Clock Divider, Sequencer
-if (went_up){
-  for (int i = 0; i <= 3; i++) { 
-    
-    if ((clockCount+division[i]-1) % division[i] == 0){
-    digitalWrite(pin_CLK_DIV_OUT[i],HIGH);
-      seq_step = (((clockCount-1)/division[i])%4)+1;
-      patternDriver(pattern[i],seq_step,pin_A[i],pin_B[i]);
-    }
+  analogReference(DEFAULT);
+  pinMode(pin_CLOCK, INPUT);
+  for (int i = 0; i <= 3; i++) {
+    pinMode(pin_A[i], OUTPUT); pinMode(pin_B[i], OUTPUT); 
+    pinMode(pin_CLK_DIV_OUT[i], OUTPUT);
+    //pinMode(pin_PTRN[i],INPUT); pinMode(pin_CLK_DIV_IN[i],INPUT);
+    clock_div_read = analogRead(pin_CLK_DIV_IN[i]);
+    mapped_div_ind = constrain(map(clock_div_read, 0, 922, 0, 9), 0, 9);
+    division[i] = divisions[mapped_div_ind];
+    clock_div_mid[i] = 51 + (1024 / 10) * mapped_div_ind;
   }
 }
 
-if (went_down){
-  for (int i = 0; i <= 3; i++){
-    digitalWrite(pin_CLK_DIV_OUT[i],LOW);
-        // Pattern reader
-    pattern_read = analogRead(pin_PTRN[i])+ptrn_size/2;
-    
-    if (abs(pattern_read-ptrn_mid[i])>20){
-      // -5:0 V
-    if      (pattern_read >= 129 & pattern_read < 309) {pattern[i] = map(pattern_read,129,279,1,6); ptrn_mid[i] = ptrn_offset -6*ptrn_size + ptrn_size/2 + (pattern[i]-1)*ptrn_size;}
-     // 0:5 V
-    else if (pattern_read >= 309 & pattern_read < 489) {pattern[i] = map(pattern_read,309,459,1,6); ptrn_mid[i] = ptrn_offset +0*ptrn_size + ptrn_size/2 + (pattern[i]-1)*ptrn_size;}
-     // 5:10 V
-    else if (pattern_read >= 489 & pattern_read < 669) {pattern[i] = map(pattern_read,489,639,1,6); ptrn_mid[i] = ptrn_offset +6*ptrn_size + ptrn_size/2 + (pattern[i]-1)*ptrn_size;}
-     // 10:15V
-    else if (pattern_read >= 669 & pattern_read < 849) {pattern[i] = map(pattern_read,669,819,1,6); ptrn_mid[i] = ptrn_offset+12*ptrn_size + ptrn_size/2 + (pattern[i]-1)*ptrn_size;}
-                                          }
-       // Clock divider reader
-    clock_div_read = analogRead(pin_CLK_DIV_IN[i]);
-    if (abs(clock_div_read-clock_div_mid[i])>54){
-    mapped_div_ind   = constrain(map(clock_div_read,0,922,0,9),0,9);
-    division[i]      = divisions[mapped_div_ind];
-    clock_div_mid[i] = 51+(1024/10)*mapped_div_ind;}
-    
-    }}
+void loop() {
+  // Reset input
+  if (digitalRead(pin_RESET) == HIGH) {
+    if (wasReset == false) {
+      clockCount = 0;
+      wasReset = true;
+    }
+  } else {
+    wasReset = false;
+  }
   
+  // Clock input
+  curr_clock_state = digitalRead(pin_CLOCK);
+  if (curr_clock_state == 1 & last_clock_state == 0) {
+    went_up = true;
+    if (clockCount < 3360) { //3360 can be divided by the numbers in divisions[]
+      clockCount++;
+    } else {
+      clockCount = 1;
+    }
+  } else if (curr_clock_state == 0 & last_clock_state == 1) {
+    went_down = true;
+  } else { 
+    went_up = false; 
+    went_down = false;
+  }
+  last_clock_state = curr_clock_state;
+
+  // Clock Divider, Sequencer
+  if (went_up){
+    for (int i = 0; i <= 3; i++) { 
+      if ((clockCount + division[i] - 1) % division[i] == 0) {
+        digitalWrite(pin_CLK_DIV_OUT[i], HIGH);
+        seq_step = (((clockCount - 1)/division[i]) % 4) + 1;
+        patternDriver(pattern[i], seq_step, pin_A[i], pin_B[i]);
+      }
+    }
+  }
+
+  if (went_down) {
+    for (int i = 0; i <= 3; i++){
+      digitalWrite(pin_CLK_DIV_OUT[i], LOW);
+    
+      // Pattern reader
+      pattern_read = analogRead(pin_PTRN[i]) + ptrn_size / 2;
+      
+      if (abs(pattern_read-ptrn_mid[i]) > 20) {
+        if (pattern_read >= 129 & pattern_read < 309) {
+          // -5:0 V
+          pattern[i] = map(pattern_read, 129, 279, 1, 6); 
+          ptrn_mid[i] = ptrn_offset - 6 * ptrn_size + ptrn_size / 2 + (pattern[i] - 1) * ptrn_size;
+        } else if (pattern_read >= 309 & pattern_read < 489) {
+          // 0:5 V
+          pattern[i] = map(pattern_read, 309, 459, 1, 6); 
+          ptrn_mid[i] = ptrn_offset + 0 * ptrn_size + ptrn_size / 2 + (pattern[i] - 1) * ptrn_size;
+        } else if (pattern_read >= 489 & pattern_read < 669) {
+          // 5:10 V
+          pattern[i] = map(pattern_read, 489, 639, 1, 6); 
+          ptrn_mid[i] = ptrn_offset + 6 * ptrn_size + ptrn_size / 2 + (pattern[i] - 1) * ptrn_size;
+        } else if (pattern_read >= 669 & pattern_read < 849) {
+          // 10:15V
+          pattern[i] = map(pattern_read,669,819,1,6); 
+          ptrn_mid[i] = ptrn_offset + 12 * ptrn_size + ptrn_size / 2 + (pattern[i] - 1) * ptrn_size;
+        }
+      }
+      
+      // Clock divider reader
+      clock_div_read = analogRead(pin_CLK_DIV_IN[i]);
+      if (abs(clock_div_read-clock_div_mid[i]) > 54) {
+        mapped_div_ind = constrain(map(clock_div_read, 0, 922, 0, 9), 0, 9);
+        division[i] = divisions[mapped_div_ind];
+        clock_div_mid[i] = 51 + (1024 / 10) * mapped_div_ind;
+      }
+    }
+  } 
 }
 
 void patternDriver(int pattern, int seq_step, int pinA, int pinB){
@@ -117,6 +138,6 @@ void patternDriver(int pattern, int seq_step, int pinA, int pinB){
                          HIGH, LOW,   // step 3 - B, A
                          HIGH, HIGH}; // step 4 - B, A
                          
-  digitalWrite(pinA, step_states[2*(patterns[4*(pattern-1)+seq_step-1]-1)+1]);   
-  digitalWrite(pinB, step_states[2*(patterns[4*(pattern-1)+seq_step-1]-1)  ]);
+  digitalWrite(pinA, step_states[2 * (patterns[4 * (pattern - 1) + seq_step - 1] - 1) + 1]);   
+  digitalWrite(pinB, step_states[2 * (patterns[4 * (pattern - 1) + seq_step - 1] - 1)]);
 }
